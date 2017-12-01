09322c616611399d5f7b1d62ed809785e799f253
高吉策 <gaojc@yunwutech.com>
2017-12-02 01:18:11 +0800

    update

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-
+set -ex
 # update the system
+sed -i "s|archive.ubuntu|mirrors.aliyun|g" /etc/apt/sources.list
+sed -i "/^.*security/d" /etc/apt/sources.list
+
 apt-get update
-apt-get upgrade
+apt-get -y upgrade
 
 ################################################################################
 # Install the mandatory tools
@@ -12,43 +15,38 @@ export LANGUAGE='zh_CN.UTF-8'
 export LANG='zh_CN.UTF-8'
 export LC_ALL='zh_CN.UTF-8'
 locale-gen zh_CN.UTF-8
-dpkg-reconfigure locales
+##dpkg-reconfigure locales
 
 # install utilities
-apt-get -y install vim git zip bzip2 fontconfig curl language-pack-en
+apt-get -y install vim git zip bzip2 fontconfig curl language-pack-zh-hans
 
 # install Java 8
-apt-get install openjdk-8-jdk
+apt-get -y install openjdk-8-jdk
 
 # install node.js
-curl -sL https://deb.nodesource.com/setup_8.x | bash -
-apt-get install -y nodejs unzip python g++ build-essential
 
-# update npm
-npm install -g npm
+##su -c "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash" ubuntu
+
+##su -c 'source ~/.bashrc && nvm install 6.9' ubuntu
+
+apt-get install -y unzip python g++ build-essential
+
 
 # install yarn
-npm install -g yarn
-su -c "yarn config set prefix /home/vagrant/.yarn-global" vagrant
+#npm install -g yarn
+#su -c "yarn config set prefix /home/ubuntu/.yarn-global" ubuntu
 
-# install yeoman grunt bower gulp
-su -c "yarn global add yo bower gulp" vagrant
 
-# install JHipster
-su -c "yarn global add generator-jhipster@4.11.1" vagrant
-
-# install JHipster UML
-su -c "yarn global add jhipster-uml@2.0.3" vagrant
 
 ################################################################################
 # Install the graphical environment
 ################################################################################
 
 # force encoding
-echo 'LANG=en_US.UTF-8' >> /etc/environment
-echo 'LANGUAGE=en_US.UTF-8' >> /etc/environment
-echo 'LC_ALL=en_US.UTF-8' >> /etc/environment
-echo 'LC_CTYPE=en_US.UTF-8' >> /etc/environment
+echo 'LANG=zh_CN.UTF-8' >> /etc/environment
+echo 'LANGUAGE=zh_CN.UTF-8' >> /etc/environment
+echo 'LC_ALL=zh_CN.UTF-8' >> /etc/environment
+echo 'LC_CTYPE=zh_CN.UTF-8' >> /etc/environment
 
 # run GUI as non-privileged user
 echo 'allowed_users=anybody' > /etc/X11/Xwrapper.config
@@ -59,10 +57,6 @@ apt-get install -y xubuntu-desktop virtualbox-guest-dkms virtualbox-guest-utils 
 # remove light-locker (see https://github.com/jhipster/jhipster-devbox/issues/54)
 apt-get remove -y light-locker --purge
 
-# change the default wallpaper
-#wget https://jhipster.github.io/images/wallpaper-004-2560x1440.png -O /usr/share/xfce4/backdrops/jhipster-wallpaper.png
-wget https://raw.githubusercontent.com/jhipster/jhipster-devbox/master/images/jhipster-wallpaper.png -O /usr/share/xfce4/backdrops/jhipster-wallpaper.png
-sed -i -e 's/xubuntu-wallpaper.png/jhipster-wallpaper.png/' /etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
 
 ################################################################################
 # Install the development tools
@@ -73,7 +67,7 @@ sed -i -e 's/xubuntu-wallpaper.png/jhipster-wallpaper.png/' /etc/xdg/xdg-xubuntu
 add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make
 
 apt-get update
-apt-get upgrade
+apt-get -y upgrade
 
 apt install -y ubuntu-make
 
@@ -83,65 +77,92 @@ apt-get install -y chromium-browser
 # install MySQL Workbench
 apt-get install -y mysql-workbench
 
-# install PgAdmin
-apt-get install -y pgadmin3
-
-# install Heroku toolbelt
-wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-
 # install Guake
 apt-get install -y guake
 cp /usr/share/applications/guake.desktop /etc/xdg/autostart/
 
-# install jhipster-devbox
-git clone git://github.com/jhipster/jhipster-devbox.git /home/vagrant/jhipster-devbox
-chmod +x /home/vagrant/jhipster-devbox/tools/*.sh
 
 # install zsh
 apt-get install -y zsh
 
 # install oh-my-zsh
-git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
-cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc
-chsh -s /bin/zsh vagrant
+git clone git://github.com/robbyrussell/oh-my-zsh.git /home/ubuntu/.oh-my-zsh
+cp /home/ubuntu/.oh-my-zsh/templates/zshrc.zsh-template /home/ubuntu/.zshrc
+chsh -s /bin/zsh ubuntu
 echo 'SHELL=/bin/zsh' >> /etc/environment
 
 # install jhipster-oh-my-zsh-plugin
-git clone https://github.com/jhipster/jhipster-oh-my-zsh-plugin.git /home/vagrant/.oh-my-zsh/custom/plugins/jhipster
-sed -i -e "s/plugins=(git)/plugins=(git docker docker-compose jhipster)/g" /home/vagrant/.zshrc
-echo 'export PATH="$PATH:/usr/bin:/home/vagrant/.yarn-global/bin:/home/vagrant/.yarn/bin:/home/vagrant/.config/yarn/global/node_modules/.bin"' >> /home/vagrant/.zshrc
+git clone https://github.com/jhipster/jhipster-oh-my-zsh-plugin.git /home/ubuntu/.oh-my-zsh/custom/plugins/jhipster
+sed -i -e "s/plugins=(git)/plugins=(git docker docker-compose jhipster)/g" /home/ubuntu/.zshrc
+echo 'export PATH="$PATH:/usr/bin:/home/ubuntu/.yarn-global/bin:/home/ubuntu/.yarn/bin:/home/ubuntu/.config/yarn/global/node_modules/.bin"' >> /home/ubuntu/.zshrc
 
-# change user to vagrant
-chown -R vagrant:vagrant /home/vagrant/.zshrc /home/vagrant/.oh-my-zsh
+# change user to ubuntu
+chown -R ubuntu:ubuntu /home/ubuntu/.zshrc /home/ubuntu/.oh-my-zsh
 
 # install Visual Studio Code
-su -c 'umake ide visual-studio-code /home/vagrant/.local/share/umake/ide/visual-studio-code --accept-license' vagrant
+su -c 'umake ide visual-studio-code /home/ubuntu/.local/share/umake/ide/visual-studio-code --accept-license' ubuntu
 
 # fix links (see https://github.com/ubuntu/ubuntu-make/issues/343)
-sed -i -e 's/visual-studio-code\/code/visual-studio-code\/bin\/code/' /home/vagrant/.local/share/applications/visual-studio-code.desktop
+sed -i -e 's/visual-studio-code\/code/visual-studio-code\/bin\/code/' /home/ubuntu/.local/share/applications/visual-studio-code.desktop
 
 # disable GPU (see https://code.visualstudio.com/docs/supporting/faq#_vs-code-main-window-is-blank)
-sed -i -e 's/"$CLI" "$@"/"$CLI" "--disable-gpu" "$@"/' /home/vagrant/.local/share/umake/ide/visual-studio-code/bin/code
+sed -i -e 's/"$CLI" "$@"/"$CLI" "--disable-gpu" "$@"/' /home/ubuntu/.local/share/umake/ide/visual-studio-code/bin/code
 
 #install IDEA community edition
-su -c 'umake ide idea /home/vagrant/.local/share/umake/ide/idea' vagrant
+#su -c 'umake ide idea /home/ubuntu/.local/share/umake/ide/idea' ubuntu
 
 # increase Inotify limit (see https://confluence.jetbrains.com/display/IDEADEV/Inotify+Watches+Limit)
 echo "fs.inotify.max_user_watches = 524288" > /etc/sysctl.d/60-inotify.conf
 sysctl -p --system
 
 # install latest Docker
-curl -sL https://get.docker.io/ | sh
+curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+# Step 3: 写入软件源信息
+sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+# Step 4: 更新并安装 Docker-CE
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
+mkdir -p /etc/docker
+tee /etc/docker/daemon.json <<-'EOF'
+{
+"registry-mirrors": ["https://k3hybo1a.mirror.aliyuncs.com"]
+}
+EOF
+systemctl daemon-reload
+systemctl restart docker
 
 # install latest docker-compose
 curl -L "$(curl -s https://api.github.com/repos/docker/compose/releases | grep browser_download_url | head -n 4 | grep Linux | cut -d '"' -f 4)" > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # configure docker group (docker commands can be launched without sudo)
-usermod -aG docker vagrant
+usermod -aG docker ubuntu
 
 # fix ownership of home
-chown -R vagrant:vagrant /home/vagrant/
+chown -R ubuntu:ubuntu /home/ubuntu/
+
+# ----------------------------------------------------------------
+# Install Golang
+# ----------------------------------------------------------------
+GO_VER=1.7.5
+GO_URL=https://storage.googleapis.com/golang/go${GO_VER}.linux-amd64.tar.gz
+
+# Set Go environment variables needed by other scripts
+export GOPATH="/opt/gopath"
+export GOROOT="/opt/go"
+PATH=$GOROOT/bin:$GOPATH/bin:$PATH
+
+cat <<EOF >/etc/profile.d/goroot.sh
+export GOROOT=$GOROOT
+export GOPATH=$GOPATH
+export PATH=\$PATH:$GOROOT/bin:$GOPATH/bin
+EOF
+
+mkdir -p $GOROOT
+mkdir -p $GOPATH
+
+curl -sL $GO_URL | (cd $GOROOT && tar --strip-components 1 -xz)
+chown -R ubuntu:ubuntu $GOPATH
 
 # clean the box
 apt-get -y autoclean
